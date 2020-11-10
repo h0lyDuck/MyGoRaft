f13bdd5b363cab336dd26c702e882f8d22c3ac17
h0lyDuck <46277346+h0lyDuck@users.noreply.github.com>
2020-11-10 12:05:19 +0800

    部分rpc完成

--- main.go
+++ main.go
@@ -1,3 +1,18 @@
 package MyGoRaft
 
 var heartBeatTimeout = 3
+
+var nodeList map[string]string
+var nodeNum int
+
+func main() {
+	nodeList = map[string]string{
+		"n1": ":10001",
+		"n2": ":10002",
+		"n3": ":10003",
+		"n4": ":10004",
+		"n5": ":10005",
+		"n6": ":10006",
+	}
+	nodeNum = len(nodeList)
+}
--- node.go
+++ node.go
@@ -1,11 +1,14 @@
 package MyGoRaft
 
 import (
+	"MyGoRaft/NodeRPC"
+	"context"
+	"google.golang.org/grpc"
+	"log"
 	"sync"
 	"time"
 )
 
-//声明raft节点类型
 type NodeInfo struct {
 	id   string
 	port string
@@ -23,7 +26,7 @@ type Node struct {
 	lastMessageTime   int64
 	nextHeartBeatTime int64
 	heartBeatTimeout  int
-	voteCh            chan bool
+	voteResult        chan bool
 	heartBeat         chan bool
 }
 
@@ -39,6 +42,7 @@ func initNode(id, port string) *Node {
 	n.lastMessageTime = 0
 	n.nextHeartBeatTime = 0
 	n.heartBeatTimeout = heartBeatTimeout
+	n.voteResult = make(chan bool)
 	return n
 }
 
@@ -46,12 +50,91 @@ func (n *Node) becomeCandidate() bool {
 	sleepTime := randRange(1500, 5000)
 	time.Sleep(time.Duration(sleepTime) * time.Microsecond)
 	if n.role == 0 && n.votedFor != "-1" && n.leader != "-1" {
-
+		n.setRole(1)
+		n.setVotedFor("-1")
+		n.setTerm(n.term + 1)
+		n.setLeader("-1")
+		n.encVote()
+		log.Println("超时后无人参选，本节点" + n.id + "变成候选人")
 		return true
 	}
 	return false
 }
 
+func (n *Node) becomeLeader() bool {
+	log.Println("本节点开始参加选举")
+	go func() {
+		for key, port := range nodeList {
+			if key != n.id {
+				conn, err := grpc.Dial("127.0.0.1"+port, grpc.WithInsecure())
+				if err != nil {
+					log.Panic("连接出错啦")
+				}
+				c := NodeRPC.NewRaftNodeClient(conn)
+				req := NodeRPC.IDRequest{
+					ID: n.id,
+				}
+				ret, err := c.Vote(context.Background(), &req)
+				if err != nil {
+					log.Panic("远程调用出错啦")
+				}
+				if ret.Result {
+					n.encVote()
+				}
+				conn.Close()
+			}
+			if n.vote > nodeNum/2 && n.leader == "-1" {
+				log.Println("获得超半数的选票,变成leader")
+				n.setRole(2)
+				n.setLeader(n.id)
+				for key, port := range nodeList {
+					if key != n.id {
+						conn, err := grpc.Dial("127.0.0.1"+port, grpc.WithInsecure())
+						if err != nil {
+							log.Panic("连接出错啦")
+						}
+						c := NodeRPC.NewRaftNodeClient(conn)
+						req := NodeRPC.IDRequest{
+							ID: n.id,
+						}
+						ret, err := c.ConfirmLeader(context.Background(), &req)
+						if err != nil {
+							log.Panic("远程调用出错啦")
+						}
+						if ret.Result {
+							log.Println("节点" + key + "已确认本节点为领导者")
+						}
+						conn.Close()
+					}
+				}
+				n.voteResult <- true
+				return
+			}
+		}
+	}()
+	select {
+	case <-time.After(time.Duration(heartBeatTimeout) * time.Second):
+		log.Println("领导人选举超时，本节点变为Follower状态")
+		n.reDefault()
+		return false
+	case success := <-n.voteResult:
+		if success {
+			return true
+		}
+		return false
+	}
+}
+
+//
+// 节点gRPC part
+//
+func (n *Node) Vote(ctx context.Context, req *NodeRPC.IDRequest) (*NodeRPC.BoolResponse, error) {
+
+}
+
+//
+// 节点修改自身属性 part
+//
 func (n *Node) setVotedFor(new string) {
 	n.lock.Lock()
 	n.votedFor = new
